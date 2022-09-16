94403eb16e7af757b231a6d6fe196b150ce746cd
Anthony Grieco <83081445+agrieco20@users.noreply.github.com>
2022-09-15 20:33:07 -0400

    Add files via upload

--- Stacks and Queues with a Linked List (C)/stack.c
+++ Stacks and Queues with a Linked List (C)/stack.c
@@ -12,34 +12,57 @@
 
 #include "linked_list.h"
 
-void stackPush(int changeNodeNum, int *nodeCount, int userInput_int/*, struct listNode * ptr, struct listNode * nodeElem*/) {
-//    changeNodeNum = 0; //Reset
-//    do {
-//        if(changeNodeNum < 0){
-//            printf("You have entered a negative number. Please try again.\n");
-//            changeNodeNum = 0; //Reset
-//        }
-//        printf("How many nodes would you like added to the Linked List as if it were a STACK? \n");
-//        scanf("%d", &changeNodeNum);
-//        nodeCount += changeNodeNum;
-//    } while(changeNodeNum < 0);
-//
-//    //Following code assigns the node values the user wants to be added to the Linked List as a STACK
-//    struct listNode * newNode; //Acts as a placeholder node so that the new information the user wants entered as its own node in the Linked List can be added
-//    struct listNode tempNode; //Acts as an initializer for temporarily holding the new node created using the "createNewNodes()" function (the numeric value [as sent by the user] and the next address [the TAIL of the Linked List] are then sent to the "newNode" listNode structure so that the previous node can be updated with the current node's address)
-//
-//    for(int i=0; i<changeNodeNum; i++){
-//        printf("Enter an integer to be added to the Linked List as if it were a STACK: \n");
-//        scanf("%d", &userInput_int);
-//
-//        newNode = (struct listNode *) malloc((sizeof(struct listNode)));
-//        tempNode = createNewNode(userInput_int, nodeElem->nextNode); //Creates a new node with the value the user has specified
-//        newNode->value = tempNode.value;
-//        newNode->nextNode = tempNode.nextNode;
-//        nodeElem->nextNode = newNode; //Reassigns the "next" address being pointed to at the new node [newNode] just created to house the user's requested data for the Linked List
-////        printf("New Node Value: %d\n", newNode->value); //Temporary
-////        printf("New Node Address: %p\n", newNode->nextNode); //Temporary
-//    }
-//
-//    displayNodes(nodeElem, ptr, *nodeCount); //Prints out all the node values currently being held by the Linked List
+//struct listNode * nodeElem;
+//struct listNode * ptr;
+
+void stackPush(int changeNodeNum, int * nodeCount, int userInput_int, struct listNode * ptr, struct listNode * nodeElem) {
+    changeNodeNum = 0; //Reset
+    do {
+        if(changeNodeNum < 0){
+            printf("You have entered a negative number. Please try again.\n");
+            changeNodeNum = 0; //Reset
+        }
+        printf("How many nodes would you like added to the Linked List as if it were a STACK? \n");
+        scanf("%d", &changeNodeNum);
+        *nodeCount += changeNodeNum;
+    } while(changeNodeNum < 0);
+
+    //Following code assigns the node values the user wants to be added to the Linked List as a STACK
+    struct listNode * newNode; //Acts as a placeholder node so that the new information the user wants entered as its own node in the Linked List can be added
+    struct listNode tempNode; //Acts as an initializer for temporarily holding the new node created using the "createNewNodes()" function (the numeric value [as sent by the user] and the next address [the TAIL of the Linked List] are then sent to the "newNode" listNode structure so that the previous node can be updated with the current node's address)
+
+    for(int i=0; i<changeNodeNum; i++){
+        printf("Enter an integer to be added to the Linked List as if it were a STACK: \n");
+        scanf("%d", &userInput_int);
+
+        newNode = (struct listNode *) malloc((sizeof(struct listNode)));
+        tempNode = createNewNode(userInput_int, nodeElem->nextNode); //Creates a new node with the value the user has specified
+        newNode->value = tempNode.value;
+        newNode->nextNode = tempNode.nextNode;
+        nodeElem->nextNode = newNode; //Reassigns the "next" address being pointed to at the new node [newNode] just created to house the user's requested data for the Linked List
+//        printf("New Node Value: %d\n", newNode->value); //Temporary
+//        printf("New Node Address: %p\n", newNode->nextNode); //Temporary
+    }
+
+    displayNodes(nodeElem, ptr, *nodeCount); //Prints out all the node values currently being held by the Linked List
+}
+
+void stackPop(int changeNodeNum, int * nodeCount, struct listNode * ptr, struct listNode * nodeElem){
+    changeNodeNum = 0; //Reset
+    do {
+        if (changeNodeNum > *nodeCount || changeNodeNum < 0){
+            printf("You have entered a value that either exceeds the number of nodes currently in the Linked List or is a negative value.\nPlease try again.\n");
+            changeNodeNum = 0; //Reset
+        }
+        printf("How many nodes would you like removed from the Linked List as if it were a STACK? \n");
+        scanf("%d", &changeNodeNum);
+    } while (changeNodeNum > *nodeCount || changeNodeNum < 0);
+    *nodeCount -= changeNodeNum;
+
+    for(int i=0;i<changeNodeNum;i++) {
+        linkedList_Decrementor(nodeElem);
+    }
+
+    displayNodes(nodeElem, ptr, *nodeCount); //Prints out all the node values currently being held by the Linked List
+
 }
--- Stacks and Queues with a Linked List (C)/stack.h
+++ Stacks and Queues with a Linked List (C)/stack.h
@@ -5,11 +5,18 @@
  * File Description: Provides all the required data declaration and function header information of the Linked List structure in relation to all given "Stack" options (ex: PUSH and POP)
  */
 
+//#include "linked_list.h"
+
 #ifndef C_PROJECT_STACK_H
 #define C_PROJECT_STACK_H
 
 #endif //C_PROJECT_STACK_H
 
-void stackPush(int changeNodeNum, int * nodeCount, int userInput_int/*, struct listNode * ptr, struct listNode * nodeElem*/);
+//Below function is necessary so that the rest of the Stack functions understand how a "struct listNode" is built and the potential data it can hold
+struct listNode createNewNode(int value, struct listNode *nextNode); //Creates a new node which will be added to the Linked List in a manner dependent upon whether the user is currently adding elements to the Linked List with a Stack or a Queue
 
-//void stackPop();
+//Responsible for adding nodes to the Linked List as if it were a STACK
+void stackPush(int changeNodeNum, int * nodeCount, int userInput_int, struct listNode * ptr, struct listNode * nodeElem);
+
+//Responsible for removing nodes from the Linked List as if it were a STACK
+void stackPop(int changeNodeNum, int * nodeCount, struct listNode * ptr, struct listNode * nodeElem);
